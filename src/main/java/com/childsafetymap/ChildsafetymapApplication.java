ee8fdd4d21a21e95cf0374df0aa2a53a3d0b5154
ichubtou <msjj0809@naver.com>
2023-08-25 01:30:22 +0900

    [Init] spring initialization

---
+++ src/main/java/com/childsafetymap/ChildsafetymapApplication.java
@@ -0,0 +1,13 @@
+package com.childsafetymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildsafetymapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildsafetymapApplication.class, args);
+	}
+
+}
