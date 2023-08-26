f43334b36317d2d4312dcb267ba67aa63bae4934
ichubtou <msjj0809@naver.com>
2023-08-26 21:39:10 +0900

    [Feat] Tag entity 생성 #1

---
+++ src/main/java/com/childsafetymap/tag/entity/Tag.java
@@ -0,0 +1,53 @@
+package com.childsafetymap.tag.entity;
+
+import com.childsafetymap.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long tagId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TagKind kind;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TagCategory tagCategory;
+
+    public enum TagKind {
+        SAFE("안전"),
+        DANGER("위험");
+
+        @Getter
+        public String isSafe;
+
+        TagKind(String isSafe) {
+            this.isSafe = isSafe;
+        }
+    }
+
+    public enum TagCategory {
+        POLICE_STATION("경찰서"),
+        CONSTRUCTION_SITE("공사장");
+
+        @Getter
+        public String category;
+
+        TagCategory(String category) {
+            this.category = category;
+        }
+    }
+}
