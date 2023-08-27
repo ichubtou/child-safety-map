0f5258786f77cc823321f2b6c4ca16c8e5103fc9
ichubtou <msjj0809@naver.com>
2023-08-27 21:55:45 +0900

    [Feat] Marker Controller 생성 #5

---
+++ src/main/java/com/childsafetymap/marker/controller/MarkerController.java
@@ -0,0 +1,39 @@
+package com.childsafetymap.marker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MarkerController {
+
+    @PostMapping
+    public ResponseEntity<?> createMarker() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping("/{markerId}")
+    public ResponseEntity<?> getMarker(@PathVariable String markerId) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getMarkers() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateMarkers() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteMarker() {
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUserMarkers(@PathVariable String userId) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+}
