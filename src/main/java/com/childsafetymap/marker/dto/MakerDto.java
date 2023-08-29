a498ee6261d3f8a3e2b926567ee322b8ea6640b2
ichubtou <msjj0809@naver.com>
2023-08-29 14:58:54 +0900

    [Feat] MarkerDto Validation 적용 #14

--- src/main/java/com/childsafetymap/marker/dto/MakerDto.java
+++ src/main/java/com/childsafetymap/marker/dto/MakerDto.java
@@ -1,25 +1,55 @@
 package com.childsafetymap.marker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 public class MakerDto {
 
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Post {
 
+        @NotNull
         private boolean isSafe;
+
+        @NotNull
         private String tagKind;
+
+        @Pattern(regexp = "^{2,10}$", message = "태그 이름은 2자 이상 10자 이하여야 합니다.")
+        @NotNull
         private String tagName;
+
+        @Pattern(regexp = "^{2,10}$", message = "마커 설명은 2자 이상 200자 이하여야 합니다.")
+        @NotNull
         private String description;
+
+        @NotNull
         private double longitude;
+
+        @NotNull
         private double latitude;
 
     }
 
     public static class Patch {
 
+        @NotNull
         private boolean isSafe;
+
+        @NotNull
         private String tagKind;
+
+        @Pattern(regexp = "^{2,10}$", message = "태그 이름은 2자 이상 10자 이하여야 합니다.")
+        @NotNull
         private String tagName;
+
+
         private String description;
 
     }
