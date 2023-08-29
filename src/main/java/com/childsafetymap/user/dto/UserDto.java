5ad22cf47de9860a39619b502be3295d4f2b8d50
ichubtou <msjj0809@naver.com>
2023-08-29 21:12:36 +0900

    [Feat] UserDto Validation 적용 #14

--- src/main/java/com/childsafetymap/user/dto/UserDto.java
+++ src/main/java/com/childsafetymap/user/dto/UserDto.java
@@ -1,18 +1,32 @@
 package com.childsafetymap.user.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class UserDto {
 
     public static class Post {
 
+        @NotNull
+        @Pattern(regexp = "[0-9a-zA-Z]+(.[_a-z0-9-]+)*@(?:\\\\w+\\\\.)+\\\\w+$", message = "유효한 이메일 주소가 아닙니다.")
         private String email;
+
+        @NotNull
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 영문과 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
         private String password;
+
         private boolean isParents;
 
     }
 
     public static class Update {
 
+        @NotNull
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 영문과 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
         private String previousPassword;
+
+        @NotNull
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$", message = "비밀번호는 영문과 특수문자, 숫자를 포함하여 8자 이상, 20자 이하여야 합니다.")
         private String password;
         private String checkPassword;
 
