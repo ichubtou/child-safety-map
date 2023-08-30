3f9ad76be29bd5b6e3ee3d8dd0c3a3274dc7ee3e
ichubtou <msjj0809@naver.com>
2023-08-30 16:58:44 +0900

    [Feat] GuardianWardsRepository 생성 #28

---
+++ src/main/java/com/childsafetymap/guardianwards/repository/GuardianWardsRepository.java
@@ -0,0 +1,7 @@
+package com.childsafetymap.guardianwards.repository;
+
+import com.childsafetymap.guardianwards.entity.GuardianWards;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuardianWardsRepository extends JpaRepository<GuardianWards, Long> {
+}
